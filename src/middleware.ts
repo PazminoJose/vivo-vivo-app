6d5f2699b7a3fe80aa88331c21647a710a4ed5c6
José Pazmiño <pepe.paz0399@gmail.com>
2024-03-08 10:18:14 -0500

    feat: add middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,30 @@
-import { withAuth } from "next-auth/middleware";
+import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth({
-  callbacks: {
-    authorized: ({ req, token }) => {
-      if (req.nextUrl.pathname === "/login") return true;
-      if (!token) return false;
-      // const roles = token?.roles.map((rol) => rol.roleName.toUpperCase());
-      // const currentPathRole = req.nextUrl.pathname.split("/")[1].toLocaleUpperCase();
-      // const isRolMatch = roles?.includes(currentPathRole);
-      // return isRolMatch ?? false;
-      return true;
+export default withAuth(
+  function middleware(req: NextRequestWithAuth) {
+    const session = req.nextauth.token;
+    const hasAdminRole =
+      session &&
+      session.user.roles.some((role) => role.roleName === "ADMIN" || role.roleName === "SUPER_ADMIN");
+    if (hasAdminRole && req.nextUrl.pathname === "/login") {
+      return NextResponse.redirect(new URL("/admin/users", req.url));
+    }
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ req, token }) => {
+        if (req.nextUrl.pathname === "/login") return true;
+        if (!token) return false;
+        const isRolMatch = token?.user.roles.some(
+          (role) => role.roleName === "ADMIN" || role.roleName === "SUPER_ADMIN"
+        );
+        return isRolMatch ?? false;
+      }
     }
   }
-});
+);
 
 export const config = {
   matcher: ["/login", "/admin/:path*"]
