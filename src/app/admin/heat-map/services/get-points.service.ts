f8942ef11e7305dd638f7939daa516c384e5e9d0
José Pazmiño <pepe.paz0399@gmail.com>
2024-03-08 10:26:17 -0500

    feat: change service endpoint

--- src/app/admin/heat-map/services/get-points.service.ts
+++ src/app/admin/heat-map/services/get-points.service.ts
@@ -1,12 +1,8 @@
 import API from "@/lib/axios/api";
-import { HeatLatLngTuple } from "leaflet";
 
-export type PointsResponse = {
-  points: Array<HeatLatLngTuple>;
-};
 export async function getPointsService() {
-  const res = await API.get<PointsResponse>({
-    url: "/alarm-detail-position/alarm-status/654ad17d06e35c13d35eb659"
+  const res = await API.get<number[][]>({
+    url: "/alarm-detail-position/alarm-status/2"
   });
   return res;
 }
