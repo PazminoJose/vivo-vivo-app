0173990bb9caa5b455e7a7b4652208c3d24cd41f
José Pazmiño <pepe.paz0399@gmail.com>
2024-07-25 10:18:55 -0500

    chore: remove unused code

--- src/app/(protected)/admin/components/NavLinks/paths.ts
+++ src/app/(protected)/admin/components/NavLinks/paths.ts
@@ -1,35 +1,12 @@
 import { APP_ROLES } from "@/constants/roles";
 import {
-  IconHome,
-  IconHospital,
+  IconChartHistogram,
   IconInfoTriangle,
   IconMap2,
   IconMapPin,
   IconMapRoute,
   IconUsers
 } from "@tabler/icons-react";
-import { Link } from "./NavLinks";
-
-const adminLinks: Link[] = [
-  // { label: "Inicio", href: "/admin/home", icon: IconHome },
-  { label: "Usuarios", href: "/admin/users", icon: IconHome },
-  // { label: "Horarios policía", href: "/admin/schedules", icon: IconCalendarTime },
-  { label: "Zonas", href: "/admin/zones", icon: IconMapRoute },
-  { label: "Hospitales", href: "/admin/hospitals", icon: IconHospital },
-  // { label: "Alarmas", href: "/admin/alarms", icon: IconMapPin },
-  { label: "Mapa de calor", href: "/admin/heat-map", icon: IconMap2 }
-  // { label: "Reportes", href: "/admin/dashboard", icon: IconChartHistogram }
-];
-
-const userLinks: Link[] = [{ label: "Incidentes", href: "/user/incidents", icon: IconMap2 }];
-
-export const LINKS = {
-  [APP_ROLES.ADMIN]: adminLinks,
-  [APP_ROLES.SUPER_ADMIN]: adminLinks,
-  [APP_ROLES.POLICE]: adminLinks,
-  [APP_ROLES.USER]: userLinks,
-  [APP_ROLES.MEDIC]: userLinks
-};
 
 export type Path = {
   label: string;
@@ -118,11 +95,11 @@ export const paths: Path[] = [
     href: ROUTES.ADMIN_HEAT_MAP,
     icon: IconMap2,
     roles: routeRoles[ROUTES.ADMIN_HEAT_MAP]
+  },
+  {
+    label: "Reportes",
+    href: ROUTES.ADMIN_DASHBOARD,
+    icon: IconChartHistogram,
+    roles: routeRoles[ROUTES.ADMIN_DASHBOARD]
   }
-  // {
-  //   label: "Reportes",
-  //   href: ROUTES.ADMIN_DASHBOARD,
-  //   icon: IconChartHistogram,
-  //   roles: routeRoles[ROUTES.ADMIN_DASHBOARD]
-  // }
 ];
