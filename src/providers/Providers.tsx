7cb539ea7d0e93a64eea36849711445e4f4388f9
José Pazmiño <pepe.paz0399@gmail.com>
2024-08-17 17:52:19 -0500

    feat: add ReactQueryDevtools

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -3,6 +3,7 @@ import { theme } from "@/theme/theme";
 import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode, useState } from "react";
 import { Toaster } from "sonner";
@@ -24,6 +25,7 @@ export default function Providers({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <MantineProvider theme={theme}>
           <ModalsProvider labels={{ cancel: "Cancelar", confirm: "Confirmar" }}>
             <Toaster richColors position="bottom-center" />
