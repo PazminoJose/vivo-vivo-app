516da19dd96f7f497a9286da19a47bd474a06448
José Pazmiño <pepe.paz0399@gmail.com>
2024-08-17 17:53:41 -0500

    feat: add default value and label

--- src/components/DataSelect.tsx
+++ src/components/DataSelect.tsx
@@ -4,20 +4,35 @@ interface DataSelectProps<T> extends Omit<SelectProps, "value" | "onChange" | "d
   value?: number;
   onChange?: (value: number) => void;
   data: T[];
+  defaultLabel?: string;
+  defaultValue?: string;
   accessorLabel: keyof T;
   accessorValue: keyof T;
 }
 
 export default function DataSelect<T>(props: DataSelectProps<T>) {
-  const { value, onChange, data, accessorLabel, accessorValue, ...otherProps } = props;
+  const {
+    value,
+    onChange,
+    data,
+    accessorLabel,
+    accessorValue,
+    defaultLabel = "Seleccione una opción",
+    defaultValue = "-1",
+    ...otherProps
+  } = props;
+
   return (
     <Select
       value={value?.toString()}
       onChange={(value) => onChange?.(Number(value) || 0)}
-      data={data?.map((d) => ({
-        label: d[accessorLabel] as string,
-        value: (d[accessorValue] as number).toString()
-      }))}
+      data={[
+        { label: defaultLabel, value: defaultValue, disabled: true },
+        ...data.map((d) => ({
+          label: d[accessorLabel] as string,
+          value: (d[accessorValue] as number).toString()
+        }))
+      ]}
       {...otherProps}
     />
   );
