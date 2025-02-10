bc2f4cd0800c666a1854ca4082b27abacf0a5ada
José Pazmiño <pepe.paz0399@gmail.com>
2025-02-10 09:42:51 -0500

    feat: handle fetch errors in getImageFileFromUrl by returning null

--- src/lib/utils/getImageFileFromUrl.ts
+++ src/lib/utils/getImageFileFromUrl.ts
@@ -1,10 +1,14 @@
-export async function getImageFileFromUrl(url: string): Promise<File> {
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`Failed to fetch image, status code: ${response.status}`);
-  }
+export async function getImageFileFromUrl(url: string): Promise<File | null> {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch image, status code: ${response.status}`);
+    }
 
-  const blob = await response.blob();
-  const fileName = url.substring(url.lastIndexOf("/") + 1); // Extracting filename from URL
-  return new File([blob], fileName, { type: blob.type });
+    const blob = await response.blob();
+    const fileName = url.substring(url.lastIndexOf("/") + 1); // Extracting filename from URL
+    return new File([blob], fileName, { type: blob.type });
+  } catch (error) {
+    return null;
+  }
 }
